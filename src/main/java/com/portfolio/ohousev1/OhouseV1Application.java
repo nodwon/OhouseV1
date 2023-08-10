5ef672496fd9deabbc55ddd72596217584f2bf75
nodwon <nodwon5024@gmail.com>
2023-08-10 14:46:59 +0900

    초기 셋팅 gradle

---
+++ src/main/java/com/portfolio/ohousev1/OhouseV1Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.ohousev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhouseV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhouseV1Application.class, args);
+    }
+
+}
