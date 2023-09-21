922216b2597ef8a4278509a9841e9d9bc9e50528
nodwon <nodwon5024@gmail.com>
2023-09-21 15:42:26 +0900

    #31 css 수정

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,46 +1,46 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-
-Vue.use(VueRouter);
-
-// 컴포넌트를 import하여 사용할 수 있습니다.
-import Home from './components/Home.vue';
-import PostDetail from './components/PostDetail.vue';
-
-// 라우트 정의
-const routes = [
-    { path: '/', component: Home },
-    { path: '/posts/:id', component: PostDetail }
-];
-
-const router = new VueRouter({
-    routes
+$(() => {
+    new Detail();
 });
 
-new Vue({
-    el: '#app',
-    router, // 라우터를 Vue 인스턴스에 등록
-    data: {
-        postData: {
-            title: '',
-            content: ''
-        }
-    },
-    methods: {
-        savePost() {
-            axios.post('/posts', this.postData)
-                .then((response) => {
-                    if (response.status === 201) {
-                        // 게시물 저장 성공 시 메인 페이지로 이동
-                        this.$router.push('/');
-                    }
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        },
-        cancelPost() {
-            // 데이터 초기화 로직
-        }
+class Detail {
+    constructor() {
+        this.saveEvent();
+        this.deleteEvent();
+        // this.updateEvent(); // 필요한 경우 추가
     }
-});
+
+    saveEvent() {
+        $('#submit-button').on('click', (e) => {
+            e.preventDefault(); // 기본 동작 중단
+            let title = $('#title').val();
+            let content = $('#content').val();
+            const data = {
+                title: title,
+                content: content,
+            };
+            debugger
+
+            $.ajax({
+                type: 'POST',
+                url: '/posts',
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function () {
+                alert('글이 등록되었습니다.');
+                location.href = '/';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+            console.log(data);
+        });
+    }
+
+    deleteEvent() {
+        // 삭제 이벤트 구현
+    }
+
+    // 필요한 경우 updateEvent 메서드 추가
+}
+
+export default Detail;
