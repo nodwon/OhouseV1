2ecb1f62f53c6961999e38de04313f013ee8c59d
nodwon <nodwon5024@gmail.com>
2023-10-20 17:58:57 +0900

    실수로 지운거 다시 복구

--- src/main/java/com/portfolio/ohousev1/controller/HomeController.java
+++ src/main/java/com/portfolio/ohousev1/controller/HomeController.java
@@ -2,23 +2,31 @@ package com.portfolio.ohousev1.controller;
 
 import com.portfolio.ohousev1.dto.post.response.PostsResponse;
 import com.portfolio.ohousev1.entity.constant.SearchType;
+import com.portfolio.ohousev1.service.PaginationService;
 import com.portfolio.ohousev1.service.post.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 public class HomeController {
 
     private final PostService postService;
+    private final PaginationService paginationService;
 
-    public HomeController(PostService postService) {
+    public HomeController(PostService postService, PaginationService paginationService) {
         this.postService = postService;
+        this.paginationService = paginationService;
     }
 
     @RequestMapping("/")
@@ -60,6 +68,25 @@ public class HomeController {
         log.info("signup controller");
         return "user/sign_up";
     }
+    @GetMapping("/search")
+    public String MyPage(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        if (searchType == null && searchValue == null) {
+            Page<PostsResponse> posts = postService.searchPosts(searchType, searchValue, pageable).map(PostsResponse::from);
+            List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), posts.getTotalPages());
+            map.addAttribute("Posts", posts);
+            map.addAttribute("paginationBarNumbers", barNumbers);
+            map.addAttribute("searchTypes", SearchType.values());
+            return "posts/SearchList";
+        } else {
+            return "redirect:/search?searchOption=" + searchType + "&searchKeyword=" + searchValue;
+        }
+    }
+
 
 
 }
