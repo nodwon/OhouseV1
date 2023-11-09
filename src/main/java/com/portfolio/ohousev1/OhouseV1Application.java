157f5afa4ff21313fc57f58893441a64a8bfc2c3
nodwon <nodwon5024@gmail.com>
2023-11-09 16:33:51 +0900

    수정

--- src/main/java/com/portfolio/ohousev1/OhouseV1Application.java
+++ src/main/java/com/portfolio/ohousev1/OhouseV1Application.java
@@ -3,6 +3,7 @@ package com.portfolio.ohousev1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OhouseV1Application {
 
